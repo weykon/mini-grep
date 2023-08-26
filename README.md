303a0fcdcfc73f4350413e611fb25d8db8c26b0e
weykon <635883092@qq.com>
2023-08-26 16:48:33 +0800

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+主页https://zh.practice.rs/elegant-code-base.html，
+下一步可以mini-redis
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::process;
 fn main() {
     println!("Hello, Mini-Grep!");
 
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
